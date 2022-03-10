2ca00a0cbb6d272a37678b80beafeab86989979a
Cathy <cathyludev@gmail.com>
2022-03-10 18:37:04 -0500

    Everything without react

---
+++ client/index.js
@@ -0,0 +1,86 @@
+import React from "react";
+import ReactDom from "react-dom";
+import axios from 'axios';
+
+const HeroList = (props) => {
+    const heroes = props.heroes;
+    return (
+        <table>
+            <tbody>
+                <tr>
+                    <th>Name</th>
+                    <th>Weapon</th>
+                    <th>Game Title</th>
+                </tr>
+                {
+                    heroes.map((hero) => {
+                        return (
+                            <tr>
+                                <td>{hero.name}</td>
+                                <td>{hero.weaponType}</td>
+                                <td>{hero.gameId}</td>
+                            </tr>
+                        )
+                    })
+                }
+            </tbody>
+        </table>
+    );
+}
+const GameList = (props) => {
+    const games = props.games;
+    return (
+        <table>
+            <tbody>
+                <tr>
+                    <th>Id</th>
+                    <th>Name</th>
+                </tr>
+                {
+                    games.map((game) => {
+                        return (
+                            <tr>
+                                <td>{game.id}</td>
+                                <td>{game.name}</td>
+                            </tr>
+                        )
+                    })
+                }
+            </tbody>
+        </table>
+    );
+}
+
+class Main extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            heroes: [],
+            games: []
+        }
+    }
+
+    async componentDidMount() {
+        try {
+            const response = await axios.get('/api/heroes');
+            const heroes = response.data;
+            const gamesResponse = await axios.get('/api/games');
+            const games = gamesResponse.data;
+            this.setState({heroes, games});
+        } catch(err) {
+            console.log(err);
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Fire Emblem Heroes</h1>
+                <HeroList heroes={this.state.heroes} />
+                <GameList games={this.state.games} />
+            </div>
+        );
+    }
+}
+const root = document.querySelector('#root');
+ReactDOM.render(<Main />, root);
---
+++ db/db.js
@@ -0,0 +1,60 @@
+const Sequelize = require('sequelize');
+const db = new Sequelize('postgres://localhost/dealers_choice_react_wp', {logging: false});
+
+const Hero = db.define('hero', {
+    name: {
+        type: Sequelize.STRING(20),
+        allowNull: false
+    },
+    weaponType: {
+        type: Sequelize.ENUM('Sword', 'Lance', 'Axe', 'Tome'),
+        allowNull: false
+    }
+});
+
+const Game = db.define('game', {
+    name: {
+        type: Sequelize.STRING(40),
+        defaultValue: "Unknown",
+        allowNull: false
+    },
+    year: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    }
+});
+
+Hero.belongsTo(Game);
+Game.hasMany(Hero);
+
+Hero.getGameName = function () {
+    return this.findAll( {
+        include: [
+            {model: Game}
+        ]
+    })
+}
+
+async function seedDb() {
+    try {
+        await db.sync({force: true});
+        const FE_ME = await Game.create({name: 'Fire Emblem: Mystery of the Emblem', year: 1994});
+        const FE_BB = await Game.create({name: 'Fire Emblem: The Blazing Blade', year: 2003});
+        const FE_SS = await Game.create({name: 'Fire Emblem: The Sacred Stones', year: 2004});
+        const FE_A = await Game.create({name: 'Fire Emblem: Awakening', year: 1994});
+
+        await Hero.create({name: 'Marth', weaponType: 'Sword', gameId: FE_ME.id});
+        await Hero.create({name: 'Eliwood', weaponType: 'Sword', gameId: FE_BB.id});
+        await Hero.create({name: 'Hector', weaponType: 'Axe', gameId: FE_BB.id});
+        await Hero.create({name: 'Ephraim', weaponType: 'Lance', gameId: FE_SS.id});
+        await Hero.create({name: 'Tharja', weaponType: 'Tome', gameId: FE_A.id});
+        console.log('Seeded!');
+        
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+module.exports = {
+    db, seedDb, Hero, Game
+};
